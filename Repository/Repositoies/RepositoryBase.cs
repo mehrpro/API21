6edc1263e293a642877f91c2888810b1bee77d18
Farshid Mohammadi <fm7450@outlook.com>
2021-10-08 22:15:07 +0330

    14000717-2

---
+++ Repository/Repositoies/RepositoryBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Contracts.IServices;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.Repositoies
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T :class
+    {
+        private readonly Api21DbContext _context;
+
+        public RepositoryBase(Api21DbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> FindAll(bool trackChange)
+        {
+            return !trackChange ? _context.Set<T>().AsNoTracking() : _context.Set<T>();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
